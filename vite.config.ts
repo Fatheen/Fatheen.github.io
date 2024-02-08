86455e07a682e180d96c41836eda4d79e88ea022
Mohammed Fatheen Ahmed <37076979+Fatheen@users.noreply.github.com>
2024-02-08 15:21:36 +0000

    Added comma

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Fatheen.github.io/"
+  base: "/Fatheen.github.io/",
   plugins: [react()],
 });
