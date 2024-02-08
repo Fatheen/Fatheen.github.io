822e1044054c0463abcbb053616f42aa1828415e
CodeSandbox <support@codesandbox.io>
2024-02-08 04:03:18 +0000

    Sandbox snapshot commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,22 @@
+// src/Navbar.jsx
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <a href="#about-me">About Me</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
